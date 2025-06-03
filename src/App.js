7da8552cf89b722f07d928afd7fe0e763ee29a2b
Umadevi76 <umadevir99@gmail.com>
2025-06-03 08:30:18 -0400

    changes1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
    <Navbar />
    <Routes>
    <Route path="/" element={<Landing_Page />} />
+   <Route path="/" element={<Login />} />
+   <Route path="/" element={<Sign_Up />} />
    </Routes>
    </BrowserRouter>
    </>
